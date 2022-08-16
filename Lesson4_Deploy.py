140ef0772eac1041457923ec9e517feae38b3164
Hackdady <lenskii.vm19@physics.msu.ru>
2022-08-16 10:39:48 +0900

    contracts on ganache

--- Lesson4_Deploy.py
+++ Lesson4_Deploy.py
@@ -1,7 +1,11 @@
 from solcx import compile_standard, install_solc
 import json
 from web3 import Web3 
+from dotenv import load_dotenv
+import os
 install_solc("0.6.6")
+
+load_dotenv()
 
 with open("./Storage.sol", "r") as file:
     storage_file = file.read()
@@ -35,14 +39,32 @@ bytecode= compiled["contracts"]["Storage.sol"]["Storage"]["evm"][
 #get abi
 abi=compiled["contracts"]["Storage.sol"]["Storage"]["abi"]
 
-w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:7545"))
-chain_id=5777
-myadress="0x955da76a2E7a87d9D67c6A14969A659c53476f30"
-prkey= "04d63be94f9e476f09d26986369e1227b7d67cb1b0c9fae070940409ea83d794"
+w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:7986"))
+chain_id=1337
+myadress="0x443345069D0a30C9c6FC35eD5FE7426FfF3BB07F"
+prkey= os.getenv("PRIV_KEY")
 nonce = w3.eth.getTransactionCount(myadress)
 
 Storage= w3.eth.contract(abi=abi,bytecode=bytecode)
 
-trans= Storage.constructor().buildTransaction({"chainId":chain_id,"from":myadress,"nonce":nonce})
+trans= Storage.constructor().buildTransaction({"chainId":chain_id,"from":myadress,"gasPrice": w3.eth.gas_price,"nonce":nonce})
 
 #print(trans)
+
+signed=w3.eth.account.sign_transaction(trans,private_key=prkey)
+Thash = w3.eth.send_raw_transaction(signed.rawTransaction)
+Treceipt =w3.eth.wait_for_transaction_receipt(Thash)
+
+#Work with the contract
+
+Contract = w3.eth.contract(address=Treceipt.contractAddress, abi=abi)
+print(Contract.functions.retrieve().call())
+storetransaction = Contract.functions.str(15).build_transaction({"chainId":chain_id,"gasPrice": w3.eth.gas_price,
+"from":myadress,"nonce":nonce+1})
+print(Contract.functions.retrieve().call())
+#print(trans)
+signed_contract= w3.eth.account.sign_transaction(storetransaction, private_key =prkey )
+Tr_send= w3.eth.send_raw_transaction(signed_contract.rawTransaction)
+Tr_receipt= w3.eth.wait_for_transaction_receipt(Tr_send)
+print(Contract.functions.retrieve().call())
+
