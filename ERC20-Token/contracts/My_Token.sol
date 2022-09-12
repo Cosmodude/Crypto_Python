e3622d7c0e49d6f8f5cba2a745105f445158985b
Hackdady <lenskii.vm19@physics.msu.ru>
2022-09-12 11:29:50 +0900

    some developments

--- ERC20-Token/contracts/My_Token.sol
+++ ERC20-Token/contracts/My_Token.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract My_Token is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Gold", "My_Token") {
+    constructor(uint256 initialSupply) ERC20("My_Token", "MT") {
         _mint(msg.sender, initialSupply);
     }
 }
--- ERC20-Token/scripts/deploy_My_Token.py
+++ ERC20-Token/scripts/deploy_My_Token.py
@@ -1,14 +1,19 @@
 from scripts.helpful_scripts import get_account,get_contract
+from web3 import Web3
 from brownie import (
     My_Token,
     network,
     accounts
     )
 
+init_suply=Web3.toWei(1000,"ether")
+
 def deploy():
     acc=get_account()
-    token=My_Token.deploy(10**20,{"from":acc})
-    print(token.totalSupply())
+    token=My_Token.deploy(init_suply,{"from":acc})
+    print(token.name())
+    print(token.symbol())
+    print(token.totalSupply()/(10**18))
 
 
 def main():
