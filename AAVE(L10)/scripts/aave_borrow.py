f822450092e2d9609c9af76127f8d59d35ab42c3
Hackdady <lenskii.vm19@physics.msu.ru>
2022-09-29 15:17:31 +0900

    broblems with testnet

--- AAVE(L10)/scripts/aave_borrow.py
+++ AAVE(L10)/scripts/aave_borrow.py
@@ -1,5 +1,6 @@
 from scripts.helpful_scripts import get_account
 from scripts.get_weth import get_weth
+from web3 import Web3
 from brownie import (
     network,
     accounts,
@@ -7,22 +8,38 @@ from brownie import (
     interface
 
 )
-
+amount = Web3.toWei(0.1, "ether")
+print(amount)
 def borrow():
     acc=get_account()
     erc20_address= config["networks"][network.show_active()]["weth_token"]
     if network.show_active() in ["mainnet-fork"]:
         get_weth()
     lend_pool=get_lending_pool()
+    print(lend_pool.address)
+    approve_erc20(amount, lend_pool.address, erc20_address, acc)
+    print ("Depositing...")
+    tx=lend_pool.deposit(erc20_address, amount, acc.address, 0, {"from": acc} )
+    tx.wait(1)
+    print ("Deposited!")
+
+#Approve sending out ERC20 Tokens
+def approve_erc20(amount, spender,erc20_address, account):
+    print("proving ERC20 token...")
+    erc20= interface.IERC20(erc20_address)
+    tx= erc20.approve(spender,amount,{"from":account})
+    tx.wait(1)
+    print("Approved!")
+    return tx
 
 
 def get_lending_pool():
-    #Address  
-    lend_pool_addr_prov= interface.ILendingPoolAddressesProvider(
+    lending_pool_addresses_provider = interface.ILendingPoolAddressesProvider(
         config["networks"][network.show_active()]["lending_pool_addresses_provider"]
-        )
-    lend_pool_addr=lend_pool_addr_prov.getLendingPool()
-    #ABI
+    )
+    lending_pool_address = lending_pool_addresses_provider.getLendingPool()
+    lending_pool = interface.ILendingPool(lending_pool_address)
+    return lending_pool
 
 def main():
     borrow()
